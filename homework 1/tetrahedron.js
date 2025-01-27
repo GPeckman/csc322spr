c0ee3433fbd6c9a25c1fb3f1589f13a493bcbc7a
GPeckman <21979502+GPeckman@users.noreply.github.com>
2025-01-27 14:13:07 -0500

    Fixed up whitespace

--- homework 1/tetrahedron.js
+++ homework 1/tetrahedron.js
@@ -20,26 +20,26 @@ renderer.setClearColor(0xffffff, 0);
 const material = new THREE.LineBasicMaterial({color: 0x000000});
 function makeTriangle(p1, p2, p3) {
 	let points = [];
-  points.push(new THREE.Vector3(p1[0], p1[1], p1[2]));
+	points.push(new THREE.Vector3(p1[0], p1[1], p1[2]));
 	points.push(new THREE.Vector3(p2[0], p2[1], p2[2]));
 	points.push(new THREE.Vector3(p3[0], p3[1], p3[2]));
 	points.push(new THREE.Vector3(p1[0], p1[1], p1[2]));
-  let geometry = new THREE.BufferGeometry().setFromPoints(points);
-  let triangle = new THREE.Line(geometry, material);
-  scene.add(triangle);
+	let geometry = new THREE.BufferGeometry().setFromPoints(points);
+	let triangle = new THREE.Line(geometry, material);
+	scene.add(triangle);
 }
 
 function makeTetrahedron(p1, p2, p3, p4) {
 	let points = [p1, p2, p3, p4];
-  for (let i = 0; i < points.length; i++) {
-  	let triangleBuffer = [];
-  	for (let j = 0; j < points.length; j++) {
-    	if (i != j) {
+	for (let i = 0; i < points.length; i++) {
+		let triangleBuffer = [];
+		for (let j = 0; j < points.length; j++) {
+			if (i != j) {
 				triangleBuffer.push(points[j]);
-      }
-    }
-    makeTriangle(...triangleBuffer);
-  }
+			}
+		}
+		makeTriangle(...triangleBuffer);
+	}
 }
 
 //makeTriangle([-10, 0, 0], [0, 5, 5], [10, 0, 0])
